7109bea5c3d688e2b1c467222d43156feaa995c5
krishnakanth30598 <53077714+krishnakanth30598@users.noreply.github.com>
2019-07-19 15:53:44 +0530

    Update pgm3.py

--- pgm3.py
+++ pgm3.py
@@ -1,8 +1,8 @@
 #krishnakanth.n7@gmail.com
-alphabets=string(input())
+alphabets=input()
 if (alphbets==a,e,i,o,u):
     print("Vowels")
 elif (alphabets==b,c,d,f,g,h,j,k,l,m,n,p,q,r,s,t,v,w,x,y,z):
     print("Consonants")
- else :
- print("Invalid")
+else :
+    print("Invalid")
