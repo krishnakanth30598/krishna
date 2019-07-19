31fb912a1285af248493526697702ebfce09c4c3
krishnakanth30598 <53077714+krishnakanth30598@users.noreply.github.com>
2019-07-19 15:29:21 +0530

    Create pgm3.py

---
+++ pgm3.py
@@ -0,0 +1,7 @@
+alphabets=string(input())
+if (alphbets==a,e,i,o,u):
+    print("Vowels")
+elif (alphabets==b,c,d,f,g,h,j,k,l,m,n,p,q,r,s,t,v,w,x,y,z):
+    print("Consonants")
+ else :
+ print("Invalid")
